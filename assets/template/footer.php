ef1278cf88dc05aa28ee4f0ea328a07bb66b5f29
James Stephens <stephensja7@gmail.com>
2018-08-26 19:51:42 -0400

    created home page and cleaned up header and footer styles

--- assets/template/footer.php
+++ assets/template/footer.php
@@ -18,14 +18,13 @@
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 		</div>
 		<div class="social-menu">
-            <ul>
-                <li><a href="" title="Facebook" alt="Facebook" target="_blank"><i class="fa fa-facebook-square fa-lg" id="facebook"></i></a></li>
-                <li><a href="" title="Instagram" alt="Instagram" target="_blank"><i class="fa fa-instagram fa-lg" id="instagram"></i></a></li>
-                <li><a href="" title="Twitter" alt="Twitter" target="_blank"><i class="fa fa-twitter-square fa-lg" id="twitter"></i></a></li>
-                <li><a href="" title="Youtube" alt="Youtube" target="_blank"><i class="fa fa-youtube-square fa-lg" id="youtube"></i></a></li>
-                <li><a href="" title="Spotify" alt="Spotify" target="_blank"><i class="fa fa-spotify fa-lg" id="spotify"></i></a></li>
-                <li><a href="" title="Sound Cloud" alt="Sound Cloud" target="_blank"><i class="fa fa-soundcloud fa-lg" id="sound-cloud"></i></a></li>
-            </ul>
+            <a href="" title="Facebook" alt="Facebook" target="_blank" class="facebook"><i class="fa fa-facebook-square fa-lg" id="facebook"></i></a>
+            <a href="" title="Instagram" alt="Instagram" target="_blank" class="instagram"><i class="fa fa-instagram fa-lg" id="instagram"></i></a>
+            <a href="https://twitter.com/cc_rover_music" title="Twitter" alt="Twitter" target="_blank" class="twitter"><i class="fa fa-twitter-square fa-lg" id="twitter"></i></a>
+            <a href="https://www.youtube.com/channel/UCDDyamuDYMR3M_swqdo6lTg" title="Youtube" alt="Youtube" target="_blank" class="youtube"><i class="fa fa-youtube-square fa-lg" id="youtube"></i></a>
+            <a href="https://itunes.apple.com/us/artist/cc-rover/1409781438" title="Apple" alt="Apple" target="_blank" class="apple"><i class="fa fa-apple fa-lg" id="apple"></i></a>
+            <a href="https://open.spotify.com/artist/6UDvKZZr1QcTFEotCXV4TI" title="Spotify" alt="Spotify" target="_blank" class="spotify"><i class="fa fa-spotify fa-lg" id="spotify"></i></a>
+            <a href="" title="Sound Cloud" alt="Sound Cloud" target="_blank" class="sound-cloud"><i class="fa fa-soundcloud fa-lg" id="sound-cloud"></i></a>
 		</div>
 		<div class="copyright">
 			Copyright &copy; <?php echo date('Y'); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>.<br class="copyright-mobile-break"/> All Rights Reserved.
@@ -34,10 +33,8 @@
 			Site developed by: <a href="https://jstoobz.com" target="_blank">jstoobz</a>
 		</div>
 		<div class="extras">
-			<ul>
-				<li><a href="">Privacy Policy</a></li>
-				<li><a href="">Terms &amp; Conditions</a></li>
-			</ul>
+			<a href="">Privacy Policy</a>
+			<a href="">Terms &amp; Conditions</a>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
---
+++ assets/template/home.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Home Page */ ?>
+
+<?php get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+
+            <!-- <div class="d-flex align-items-center justify-content-center"> -->
+            <div class="home-background">
+                <img src="<?php bloginfo('template_url'); ?>/images/ccrm-background.jpg">
+            </div>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php
+get_footer();
